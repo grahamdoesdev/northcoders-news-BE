b42fb26161675f7e9a143864d1388bc0859cff32
grahamdoescode <grahamdoescode@gmail.com>
2025-07-29 22:00:11 +0100

    Try fix db

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const { Pool } = require("pg");
-const db = new Pool();
-const ENV = process.env.NODE_ENV || 'production'
-if (ENV === "production") {
-    db.connectionString = process.env.DATABASE_URL;
-    db.max = 2;
-}
+
+//const ENV = process.env.NODE_ENV || 'production'
+const ENV = "production";
+process.env.DATABASE_URL = 'postgresql://postgres.ayxajwuvuxgokxkrpcoo:graham999!@aws-0-eu-west-2.pooler.supabase.com:6543/postgres';
+
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("PGDATABASE or DATABASE_URL not set");
 }
@@ -16,12 +16,15 @@ require('dotenv').config({path: myPath})
 
 const config = {};
 
+const db = new Pool();
 
-
-
+if (ENV === "production") {
+    db.connectionString = process.env.DATABASE_URL;
+    db.max = 2;
+}
 
 if (!process.env.PGDATABASE) {
-    throw new Error("No PGDATABASE configured")
+    //throw new Error("No PGDATABASE configured")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,7 +1,7 @@
 const devData = require('../data/development-data/index.js');
 const seed = require('./seed.js');
 const db = require('../connection.js');
-
+//console.log(db);
 const runSeed = () => {
   return seed(devData).then((
   ) => {
